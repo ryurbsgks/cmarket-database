f8d171ec113a36efd9cea5df30905008e2315c67
ryurbsgks <ryurbsgks5114@gmail.com>
2022-02-21 14:20:31 +0900

    Bare minimum requirement 완료

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -16,11 +16,35 @@ module.exports = {
     get: (req, res) => {
       const userId = req.params.userId;
       // TODO: 요청에 따른 적절한 응답을 돌려주는 컨트롤러를 작성하세요.
+
+      if(!userId){
+        res.status(401).send("userId does not exist");
+      }else{
+        models.orders.get(userId, (error, result) => {
+          if (error) {
+            res.status(500).send('Internal Server Error');
+          } else {
+            res.status(200).json(result);
+          }
+        });
+      }
     },
     post: (req, res) => {
       const userId = req.params.userId;
       const { orders, totalPrice } = req.body;
       // TODO: 요청에 따른 적절한 응답을 돌려주는 컨트롤러를 작성하세요.
+      
+      if(!userId || !orders || !totalPrice){
+        res.status(400).send("Bad Request")
+      }else{
+        models.orders.post(userId, orders, totalPrice, (error, result) => {
+          if (error) {
+            res.status(500).send('Internal Server Error');
+          } else {
+            res.status(201).json(result);
+          }
+        });
+      }
     },
   },
 };
--- server/models/index.js
+++ server/models/index.js
@@ -13,12 +13,28 @@ module.exports = {
   },
   orders: {
     get: (userId, callback) => {
-      // TODO: 해당 유저가 작성한 모든 주문을 가져오는 함수를 작성하세요
-      callback(/* err, result */);
+      // TODO: 해당 유저가 작성한 모든 주문을 가져오는 함수를 
+      
+      const queryString = `SELECT orders.id, orders.created_at, orders.total_price, items.name, items.price, items.image, order_items.order_quantity FROM orders JOIN order_items ON orders.id=order_items.order_id JOIN items ON order_items.item_id=items.id WHERE orders.user_id=${userId}`;
+      
+      db.query(queryString, (error, result) => {
+        callback(error, result);
+      });
     },
     post: (userId, orders, totalPrice, callback) => {
       // TODO: 해당 유저의 주문 요청을 데이터베이스에 생성하는 함수를 작성하세요
-      callback(/* err, result */);
+      
+      const queryString = `INSERT INTO orders (user_id, total_price) VALUES (${userId}, ${totalPrice})`;
+
+      db.query(queryString, (error, result) => {
+
+        const queryString = `INSERT INTO order_items (order_id, item_id, order_quantity) VALUES ?`;
+        let param = orders.map( obj => [result.insertId, obj.itemId, obj.quantity] );
+
+        db.query(queryString, [param], (error, result) => {
+          callback(error, result);
+        });
+      });
     }
   },
 };
